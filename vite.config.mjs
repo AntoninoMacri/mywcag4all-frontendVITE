3126d546fbd907ed2b0aa6511fcd49cc22cff5f9
Antonino Macrì <antonino.mac93@gmail.com>
2024-09-11 12:30:38 +0200

    correzione direttori dei file statici (in produzione non trovava le immagini)

--- vite.config.mjs
+++ vite.config.mjs
@@ -4,6 +4,13 @@ import react from "@vitejs/plugin-react";
 export default defineConfig({
   plugins: [react()],
   server: {
-    port: 3000,
+    port: 3000, // Porta del server di sviluppo
+    open: true, // Apre automaticamente il browser
   },
+  build: {
+    outDir: "dist", // Cartella di output per il build
+    assetsDir: "assets", // Cartella in cui Vite metterà le risorse statiche nella build (es. immagini, CSS)
+    sourcemap: true, // Utile per il debug in produzione
+  },
+  base: "/accessibility-dev/", // Percorso base per l'app in produzione
 });
